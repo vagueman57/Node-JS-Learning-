8cf156af81c4ed1be324228f04deef968354a717
Rajiv Indoliya <rajiv1jan21@gmail.com>
2025-08-05 17:17:36 +0530

    commiting rest-api-development

---
+++ 4. Rest-API-development/app.js
@@ -0,0 +1,76 @@
+
+const express = require('express');
+const app = express();
+
+// middleware
+app.use(express.json());
+
+let books = [
+  {
+    id: '1',
+    title: 'Book 1'
+  },
+  {
+    id: '2',
+    title: 'Book 2'
+  },
+  {
+    id: '3',
+    title: 'Book 3'
+  }
+];
+
+// intro route
+app.get('/', (req, res) =>{
+  res.json({
+    message: 'Welcome to our bookstore api',
+  });
+});
+
+// get all books
+app.get('/get', (req, res) => {
+  res.json(books)
+});
+
+// get a single book
+app.get('/get/:id', (req, res) => {
+  const book = books.find(item => item.id === req.params.id);
+  if(book){
+    res.status(200).json(book)
+  }else{
+    res.status(404).json({
+      message: 'Book not found please try with a different Book ID'
+    });
+  }
+});
+
+// add a new book
+app.post('/add', (req, res) => {
+  const newBook = {
+    id: books.length +1,
+    title: `Book ${books.length +1}`,
+  }
+
+  books.push(newBook);
+  res.status(200).json(newBook);
+});
+
+// update a book
+app.put('/update/:id', (req, res) => {
+  const findCurrentBook = books.find(bookItem => bookItem.id === req.params.id);
+    if(findCurrentBook){
+      
+    findCurrentBook.title = req.body.title || findCurrentBook.title;
+    
+    res.status(200).json(findCurrentBook)
+  } else {
+    res.status(404).json({
+      message: 'Book not found',
+    });
+  }
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server is now running on port ${port}`);
+})
