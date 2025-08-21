991d46cc107566cbf75aca0f8493fcaa4df7a327
Rajiv Indoliya <rajiv1jan21@gmail.com>
2025-08-21 16:13:58 +0530

    last commit of 7th folder i.e. 'NodeJs-Auth'

--- 7. NodeJS-Auth/controllers/auth-controller.js
+++ 7. NodeJS-Auth/controllers/auth-controller.js
@@ -100,4 +100,54 @@ const loginUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser, loginUser}
+const changePassword = async(req, res) => {
+  try{
+    const userId = req.userInfo.userId;
+
+    // extract old and new password
+    const {oldPassword, newPassword} = req.body;
+
+    // find the current logged in user 
+    const user = await User.findById(userId);
+
+    if(!user){
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      })
+    }  
+
+    // check if the old password is correct
+    const isPasswordMatch = await bcrypt.compare(oldPassword, user.password);
+
+    if(!isPasswordMatch){
+      return res.status(400).json({
+        success: false,
+        message: "Old password is not correct! Please try again later."
+      }); 
+    }
+
+    // hash the new password here 
+    const salt = await bcrypt.genSalt(10);
+    const newHashedPassword = await bcrypt.hash(newPassword, salt);
+
+    // update the use password
+    user.password = newHashedPassword
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Password changed successfully!"
+    })
+    
+
+  } catch(e){
+    console.log(e);
+    res.status(500).json({
+      success: true, 
+      message: "Some error occured! Please try again",
+    })
+  }
+}
+
+module.exports = { registerUser, loginUser, changePassword};
--- 7. NodeJS-Auth/controllers/image-controller.js
+++ 7. NodeJS-Auth/controllers/image-controller.js
@@ -1,6 +1,7 @@
 const Image = require('../models/Image'); 
 const {uploadToCloudinary} = require('../helpers/cloudinaryHelper');
-
+const fs = require('fs');
+const cloudinary = require('../config/cloudinary');
 
 const uploadImageController = async(req, res ) => {
   try{
@@ -33,6 +34,9 @@ const uploadImageController = async(req, res ) => {
 
     await newlyUploadedImage.save();
 
+    // delete the file from local storage
+    fs.unlinkSync(req.file.path);
+
     res.status(201).json({
       success: true,
       message: "Image uploaded successfully",
@@ -46,8 +50,84 @@ const uploadImageController = async(req, res ) => {
       message: 'Something went wrong! Please try again'
      })
   }
+};
+
+const fetchImagesController = async(req, res) => {
+  try{
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 2;
+    const skip = (page - 1) * limit;
+
+    const sortBy =req.query.sortBy || 'createdAt';
+    const sortOrder = req.query.sortOrder === 'asc' ? 1 : -1;
+    const totalImages = await Image.countDocuments();
+    const totalPages = Math.ceil(totalImages / limit);
+
+    const sortObj = {};
+    sortObj[sortBy] = sortOrder;
+    const images = await Image.find().sort(sortObj).skip(skip).limit(limit);
+
+    if(images){
+      res.status(200).json({
+        success: true,
+        currentPage: page,
+        totalPages: totalPages,
+        totalImages: totalImages,
+        data: images,
+      });
+    }
+  } catch(e){
+    console.log(error);
+    res.status(500).json({
+      success: true,
+      message: "Something went wrong while fetching! Please try again",
+    })
+  }
+};
+
+const deleteImageController = async(req, res) => {
+  try{
+    const ImageToBeDeleted = req.params.id;
+    const userId = req.userInfo.userId;
+
+    const image = await Image.findById(ImageToBeDeleted);
+
+    if(!image){
+      return res.status(404).json({
+        success: false,
+        message: "Image not found"
+      })
+    }
+
+    // check if this image is uploaded bt the current user who is trying to delete this image
+    if(image.uploadedBy.toString() !== userId){
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized to delete this image"
+      })
+    }
+
+    // delete this image first from cloudinary storage
+    await cloudinary.uploader.destroy(image.publicId);
+
+    // now delete the image form mongoDB database
+    await Image.findByIdAndDelete(ImageToBeDeleted);
+
+    res.status(200).json({
+      success: true,
+      message: "Image deleted successfully"
+    })
+
+  } catch(error){
+    res.status(500).json({
+      success: false,
+      message: "something went wrong while deleting the image! Please try again",
+    })
+  }
 }
 
 module.exports = {
   uploadImageController,
+  fetchImagesController,
+  deleteImageController,
 };
